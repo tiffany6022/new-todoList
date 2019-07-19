1cebc7148bdf30ab3ae75d1a4eaa40827390b012
tiffany6022 <tiffany001510@gmail.com>
2019-07-19 18:37:10 +0800

    Add todoList first version

--- App.vue
+++ App.vue
@@ -1,14 +1,54 @@
 <template lang="pug">
 #app
   .ui.action.input
-    input(type="text")
-    button.ui.icon.button
+    input(type="text", v-model="todo")
+    button.ui.icon.button(@click="addTask(todo)")
       i.plus.icon
+
+  .ui.checkbox(v-for="task in tasks")
+    input(type="checkbox", :id="task.title", @click="toggle(task)")
+    label(:for="task.title", v-if="task.done")
+      del {{ task.title }}
+      i.close.icon(@click="deleteTask(task)")
+    label(:for="task.title", v-else) {{ task.title }}
 </template>
 
 
 <script>
-
+  export default{
+    data(){
+      return{
+        todo: "abc",
+        tasks: []
+      }
+    },
+    methods: {
+      addTask: function(tsk){
+        if(tsk){
+          this.tasks.push({
+            title: tsk,
+            done: false
+          });
+          this.todo = "";
+        }
+      },
+      toggle: function(tsk){
+        console.log(this.tasks.indexOf(tsk));
+        console.log("toggle");
+        if(tsk.done){
+          tsk.done = false;
+        }
+        else{
+          tsk.done = true;
+        }
+      },
+      deleteTask: function(tsk){
+        //console.log(this.tasks.indexOf(tsk));
+        this.tasks.splice(this.tasks.indexOf(tsk), 1);
+        //console.log(this.tasks.indexOf(tsk));
+      },
+    },
+  }
 </script>
 
 
